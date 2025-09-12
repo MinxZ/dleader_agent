150e33cadf19bc5780714ea829badf9fa95aa7e1
MinxZ <z670172581@icloud.com>
2025-09-12 16:48:01 +0900

    Create install_jp_font.sh

---
+++ install_jp_font.sh
@@ -0,0 +1,10 @@
+# Recommended - high quality fonts
+sudo apt install fonts-noto-cjk fonts-noto-cjk-extra 
+
+# OR alternative IPA fonts 
+sudo apt install fonts-ipafont fonts-ipaexfont
+
+sudo apt install fonts-noto-color-emoji fonts-symbola
+
+fc-cache -fv
+rm -rf ~/.cache/matplotlib
